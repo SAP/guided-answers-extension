eda41d20da9305e37bfc48630f92c4cbd7996a2a
Klaus Keller <klaus.keller@sap.com>
2022-04-23 16:51:05 -0700

    feat: unit test for extension.ts

--- packages/ide-extension/test/__mocks__/vscode.js
+++ packages/ide-extension/test/__mocks__/vscode.js
@@ -1,15 +1,56 @@
-const Uri = {
-    parse: jest.fn(),
-    file: (path) => new FakeUri('', '', path)
+const URI = require('vscode-uri');
+
+const commands = {
+    executeCommand: jest.fn(),
+    registerCommand: (id, handler) => handler
+};
+
+const ViewColumn = {
+    Active: -1,
+    Beside: -2,
+    One: 1,
+    Two: 2,
+    Three: 3,
+    Four: 4,
+    Five: 5,
+    Six: 6,
+    Seven: 7,
+    Eight: 8,
+    Nine: 9
 };
 
 const window = {
-    createOutputChannel: jest.fn()
+    createOutputChannel: () => {
+        appendLine: jest.fn();
+    },
+    createWebviewPanel: jest.fn().mockImplementation(() => {
+        return {
+            webview: {
+                html: '',
+                onDidReceiveMessage: jest.fn(),
+                asWebviewUri: jest.fn().mockReturnValue(''),
+                cspSource: ''
+            },
+            onDidChangeViewState: jest.fn(),
+            onDidDispose: jest.fn()
+        };
+    })
+};
+
+const workspace = {
+    getConfiguration: jest.fn().mockImplementation(() => {
+        return {
+            get: jest.fn()
+        };
+    })
 };
 
 const vscode = {
-    Uri,
-    window
+    commands,
+    Uri: URI.URI,
+    ViewColumn,
+    window,
+    workspace
 };
 
 module.exports = vscode;
--- packages/ide-extension/test/extension.test.ts
+++ packages/ide-extension/test/extension.test.ts
@@ -1,7 +1,54 @@
+import { ExtensionContext, commands, window, OutputChannel, WebviewPanel } from 'vscode';
+import * as logger from '../src/logger/logger';
 import { activate } from '../src/extension';
 
-describe('Smoke test', () => {
+describe('Extension test', () => {
     test('activate is function', () => {
         expect(typeof activate === 'function').toBeTruthy();
+    });
+
+    test('activate extension', () => {
+        // Mock setup
+        const subscriptionsMock = jest.spyOn(commands, 'registerCommand');
+        const context = {
+            subscriptions: []
+        };
+
+        // Test execution
+        activate(context as unknown as ExtensionContext);
+
+        // Result check
+        expect(subscriptionsMock.mock.calls[0][0]).toBe('sap.ux.guidedAnswer.openGuidedAnswer');
+        expect(typeof context.subscriptions[0]).toBe('function');
+    });
+
+    test('execute command', () => {
+        // Mock setup
+        const loggerMock = jest.spyOn(logger, 'logString').mockImplementation(() => null);
+        const subscriptionsMock = jest.spyOn(commands, 'registerCommand');
+        const webViewPanelMock = {
+            webview: {
+                html: '',
+                onDidReceiveMessage: jest.fn(),
+                asWebviewUri: jest.fn().mockReturnValue(''),
+                cspSource: ''
+            },
+            onDidChangeViewState: jest.fn(),
+            onDidDispose: jest.fn(),
+            reveal: jest.fn()
+        } as unknown as WebviewPanel;
+        jest.spyOn(window, 'createWebviewPanel').mockImplementation(() => webViewPanelMock);
+        const context = {
+            subscriptions: []
+        };
+
+        // Test execution
+        activate(context as unknown as ExtensionContext);
+        subscriptionsMock.mock.calls[0][1]();
+
+        // Result check
+        expect(webViewPanelMock.webview.html).toMatchSnapshot();
+        expect(webViewPanelMock.reveal).toBeCalled();
+        expect(loggerMock).toBeCalled();
     });
 });
