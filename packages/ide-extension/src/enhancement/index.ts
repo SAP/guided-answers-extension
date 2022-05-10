b1df289b199c2c9f6cf09efbc2773e882c073a78
Klaus Keller <klaus.keller@sap.com>
2022-05-09 17:51:04 -0700

    fix: linting

--- packages/ide-extension/src/enhancement/index.ts
+++ packages/ide-extension/src/enhancement/index.ts
@@ -1,4 +1,4 @@
-import { HTMLEnhancement, NodeEnhancement } from '@sap/guided-answers-extension-types';
+import type { HTMLEnhancement, NodeEnhancement } from '@sap/guided-answers-extension-types';
 import enhncmntConf from './enhancements.json';
 
 export const enhancements: { nodeEnhancements: NodeEnhancement[]; htmlEnhancements: HTMLEnhancement[] } = enhncmntConf;
