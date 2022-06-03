6487633109be6305b7b1f9b3669b071ddce05301
Sanat Thukral <sanat.thukral@sap.com>
2022-06-03 15:00:29 +0100

    file cleanup

--- packages/webapp/src/webview/types.ts
+++ packages/webapp/src/webview/types.ts
@@ -1,7 +1,6 @@
 import type { GuidedAnswerNode, GuidedAnswerTree } from '@sap/guided-answers-extension-types';
 
 export interface AppState {
-    // noAnswersFound: any;
     query: string;
     guidedAnswerTrees: GuidedAnswerTree[];
     activeGuidedAnswerNode: GuidedAnswerNode[];
