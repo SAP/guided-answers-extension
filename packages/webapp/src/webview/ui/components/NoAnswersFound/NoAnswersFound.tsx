39f25fb657e29fa7b76cdc51e031687fa0d3b320
Christos Koutsiaris <koutsiaris@gmail.com>
2022-06-10 17:20:14 +0100

    Feat/ga/unit testing setup 110 (#114)
    * feat(ga): jest configuration for react components
    
    * feat(ga): remove redundant deps
    
    * feat(ga): add i18n in component and in test
    
    * feat(ga): code review comments
    
    Co-authored-by: Christos Koutsiaris <christos.koutsiaris@sap.com>

--- packages/webapp/src/webview/ui/components/NoAnswersFound/NoAnswersFound.tsx
+++ packages/webapp/src/webview/ui/components/NoAnswersFound/NoAnswersFound.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './NoAnswersFound.scss';
 import ErrorIcon from './no-answers-found.svg';
+import i18next from 'i18next';
 
 /**
  *@returns - react element to show an error page when there are no answers found
@@ -9,8 +10,8 @@ export function NoAnswersFound() {
     return (
         <div className="error-screen">
             <div className="error-screen__objects">
-                <h1 className="error-screen__object__title">No answers found</h1>
-                <h2 className="error-screen__object__subtitle">Please modfiy search</h2>
+                <h1 className="error-screen__object__title">{i18next.t('NO_ANSWERS_FOUND')}</h1>
+                <h2 className="error-screen__object__subtitle">{i18next.t('PLEASE_MODIFY_SEARCH')}</h2>
                 <span className="error-screen__object__logo">
                     <ErrorIcon />
                 </span>
