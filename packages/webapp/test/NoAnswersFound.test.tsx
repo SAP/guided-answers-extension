338ee811bedf4008a8ade697dfa3f9d2fcdebc23
Christos Koutsiaris <christos.koutsiaris@sap.com>
2022-06-10 11:19:37 +0100

    feat(ga): add i18n in component and in test

--- packages/webapp/test/NoAnswersFound.test.tsx
+++ packages/webapp/test/NoAnswersFound.test.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { NoAnswersFound } from '../src/webview/ui/components/NoAnswersFound';
+import i18next from 'i18next';
+import { initI18n } from '../src/webview/i18n';
 
 describe('<NoAnswersFound />', () => {
     let wrapper: any;
-
+    initI18n();
     beforeEach(() => {
         wrapper = shallow(<NoAnswersFound />);
     });
@@ -16,7 +18,7 @@ describe('<NoAnswersFound />', () => {
 
     it('Should render a NoAnswersFound component', () => {
         expect(wrapper.find('.error-screen').length).toBe(1);
-        expect(wrapper.find('.error-screen__object__title').text()).toBe('No answers found');
-        expect(wrapper.find('.error-screen__object__subtitle').text()).toBe('Please modfiy search');
+        expect(wrapper.find('.error-screen__object__title').text()).toBe(i18next.t('NO_ANSWERS_FOUND'));
+        expect(wrapper.find('.error-screen__object__subtitle').text()).toBe(i18next.t('PLEASE_MODIFY_SEARCH'));
     });
 });
