450f5747b77aa592de95b8c4b0fa1ea930aa45c8
Sanat Thukral <sanat.thukral@sap.com>
2022-08-08 20:18:33 +0530

    fix(ga): tests fixed

--- packages/webapp/src/webview/ui/components/GuidedAnswerNavPath/GuidedAnswerNavPath.tsx
+++ packages/webapp/src/webview/ui/components/GuidedAnswerNavPath/GuidedAnswerNavPath.tsx
@@ -36,27 +36,25 @@ export function GuidedAnswerNavPath(): ReactElement {
                         });
                     }}
                     isCircularNavigation={true}>
-                    <div className="timeline-block-wrapper">
-                        {nodes.map((node, i) => {
-                            return (
-                                <div key={`timeline-block-${i}`} className="timeline-block">
-                                    <button
-                                        className={`timeline-content ${
-                                            i === lastIndex ? 'timeline-content-bottom-border' : ''
-                                        }`}
-                                        onClick={(): void => {
-                                            actions.updateActiveNode(node);
-                                        }}>
-                                        <div className="timeline__path" title={node.TITLE}>
-                                            <span className="timeline-content-title-small bold-text">{i + 1}</span>
-                                            <span className={`timeline-content-title-large`}>{node.TITLE}</span>
-                                        </div>
-                                    </button>
-                                    {i !== lastIndex ? <div className="vertical-line"></div> : <></>}
-                                </div>
-                            );
-                        })}
-                    </div>
+                    {nodes.map((node, i) => {
+                        return (
+                            <div key={`timeline-block-${i}`} className="timeline-block">
+                                <button
+                                    className={`timeline-content ${
+                                        i === lastIndex ? 'timeline-content-bottom-border' : ''
+                                    }`}
+                                    onClick={(): void => {
+                                        actions.updateActiveNode(node);
+                                    }}>
+                                    <div className="timeline__path" title={node.TITLE}>
+                                        <span className="timeline-content-title-small bold-text">{i + 1}</span>
+                                        <span className={`timeline-content-title-large`}>{node.TITLE}</span>
+                                    </div>
+                                </button>
+                                {i !== lastIndex ? <div className="vertical-line"></div> : <></>}
+                            </div>
+                        );
+                    })}
                 </FocusZone>
             </nav>
         );
