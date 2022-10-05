7e1b1d49e7a5c265071baf658a9ea88508a3322a
Christos Koutsiaris <christos.koutsiaris@sap.com>
2022-10-05 09:05:46 +0100

    fix(ga): typo

--- packages/webapp/src/webview/ui/components/Header/Filters/Filters.tsx
+++ packages/webapp/src/webview/ui/components/Header/Filters/Filters.tsx
@@ -259,7 +259,7 @@ export function Filters() {
                         onAccept={() => filterType[filter].apply()}
                         onCancel={resetFilter}
                         onDismiss={resetFilter}>
-                        <UITextInput placeholder="Search!" value={query} onChange={searchFilter} />
+                        <UITextInput placeholder="Search" value={query} onChange={searchFilter} />
                         <Stack
                             style={{ overflowY: 'scroll', height: '90%', padding: 0, marginTop: '10px' }}
                             tokens={verticalGapStackTokens}>
