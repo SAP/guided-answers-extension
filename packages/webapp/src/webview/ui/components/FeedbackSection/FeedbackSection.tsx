d7eaf5fba27feb3d48345607c9a1565bebbc0623
Sanat Thukral <sanat.thukral@sap.com>
2022-10-07 11:00:59 +0100

    chore(ga): feedback section design fix

--- packages/webapp/src/webview/ui/components/FeedbackSection/FeedbackSection.tsx
+++ packages/webapp/src/webview/ui/components/FeedbackSection/FeedbackSection.tsx
@@ -20,29 +20,27 @@ export function FeedbackSection(): ReactElement {
     return (
         <div className="feedback-container">
             <h3>{i18next.t('PLEASE_TELL_US_IF_THIS_ANSWER_WAS_HELPFUL')}</h3>
-            <div className="feedback-subcontainer">
-                <FocusZone direction={FocusZoneDirection.horizontal} className="feedback-subcontainer" role="tree">
-                    <button
-                        className="feedback-box solved-hover"
-                        onClick={(): void => {
-                            actions.guideFeedback(true);
-                            actions.sendFeedbackOutcome({ treeId, nodeId, solved: true });
-                        }}>
-                        <Solved />
-                        <h3>{i18next.t('THIS_SOLVED_MY_ISSUE')}</h3>
-                    </button>
-                    <button
-                        className="feedback-box not-solved-hover"
-                        style={{ border: '2px solid var(--vscode-terminal-ansiRed)' }}
-                        onClick={(): void => {
-                            actions.guideFeedback(false);
-                            actions.sendFeedbackOutcome({ treeId, nodeId, solved: false });
-                        }}>
-                        <NotSolved />
-                        <h3>{i18next.t('THIS_DID_NOT_SOLVED_MY_ISSUE')}</h3>
-                    </button>
-                </FocusZone>
-            </div>
+            <FocusZone direction={FocusZoneDirection.horizontal} className="feedback-subcontainer" role="tree">
+                <button
+                    className="feedback-box solved-hover"
+                    onClick={(): void => {
+                        actions.guideFeedback(true);
+                        actions.sendFeedbackOutcome({ treeId, nodeId, solved: true });
+                    }}>
+                    <Solved />
+                    <h3>{i18next.t('THIS_SOLVED_MY_ISSUE')}</h3>
+                </button>
+                <button
+                    className="feedback-box not-solved-hover"
+                    style={{ border: '2px solid var(--vscode-terminal-ansiRed)' }}
+                    onClick={(): void => {
+                        actions.guideFeedback(false);
+                        actions.sendFeedbackOutcome({ treeId, nodeId, solved: false });
+                    }}>
+                    <NotSolved />
+                    <h3>{i18next.t('THIS_DID_NOT_SOLVED_MY_ISSUE')}</h3>
+                </button>
+            </FocusZone>
             <MessageDialogBox
                 dialogTitle={i18next.t('THANKS')}
                 dialogText={i18next.t('THANK_YOU_TEXT')}
