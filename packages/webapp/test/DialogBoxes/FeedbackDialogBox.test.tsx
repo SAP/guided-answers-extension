aa590e04d1800146a29b82b5a9e2fab7d5c8da21
Sanat Thukral <sanat.thukral@sap.com>
2023-02-01 13:41:52 +0000

    chore: adding tests for feedbackDialogBox

---
+++ packages/webapp/test/DialogBoxes/FeedbackDialogBox.test.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { FeedbackDialogBox } from '../../src/webview/ui/components/DialogBoxes/FeedbackDialogBox';
+import { render, cleanup } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import { AppState } from '../../src/webview/types';
+import { getInitialState, reducer } from '../../src/webview/state/reducers';
+
+jest.mock('react-redux', () => ({
+    ...jest.requireActual('react-redux'),
+    useSelector: jest.fn().mockReturnValue({ feedbackStatus: true }).mockReturnValueOnce({ feedbackStatus: false })
+}));
+
+jest.mock('../src/webview/state', () => {
+    return {
+        actions: {
+            feedbackStatus: jest.fn()
+        }
+    };
+});
+
+const createState = (initialState: AppState) => (actions: any[]) => actions.reduce(reducer, initialState);
+const mockStore = configureMockStore();
+
+describe('<FeedbackDialogBox />', () => {
+    afterEach(cleanup);
+
+    const initialState = createState(getInitialState());
+    const store = mockStore(initialState);
+
+    it('Should render a FeedbackSendDialogBox component', () => {
+        const { container } = render(
+            <Provider store={store}>
+                <FeedbackDialogBox />
+            </Provider>
+        );
+        expect(container).toMatchSnapshot();
+    });
+});
