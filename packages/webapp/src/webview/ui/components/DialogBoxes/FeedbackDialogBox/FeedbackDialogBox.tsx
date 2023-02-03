09bfd485e6abbe35187b3f1fb68864887036f944
Sanat Thukral <sanat.thukral@sap.com>
2023-02-03 11:29:55 +0000

    chore: lint error fixed

--- packages/webapp/src/webview/ui/components/DialogBoxes/FeedbackDialogBox/FeedbackDialogBox.tsx
+++ packages/webapp/src/webview/ui/components/DialogBoxes/FeedbackDialogBox/FeedbackDialogBox.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useState, useEffect, FormEvent, useCallback } from 'react';
+import React, { ReactElement, useState, useEffect, FormEvent } from 'react';
 import './FeedbackDialogBox.scss';
 import { DialogFooter } from '@fluentui/react/lib/Dialog';
 import { FocusZone, FocusZoneDirection } from '@fluentui/react-focus';
