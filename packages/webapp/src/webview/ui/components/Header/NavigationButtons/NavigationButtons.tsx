f68c85bf5fee5d69e4cea038d49db6cafbeea6ab
Christos Koutsiaris <christos.koutsiaris@sap.com>
2023-02-15 16:12:07 +0000

    feat(ga): building the UI

--- packages/webapp/src/webview/ui/components/Header/NavigationButtons/NavigationButtons.tsx
+++ packages/webapp/src/webview/ui/components/Header/NavigationButtons/NavigationButtons.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { DirectionalHint } from '@fluentui/react';
 import { actions } from '../../../../state';
 import { VscHome, VscRefresh, VscArrowLeft } from 'react-icons/vsc';
 import i18next from 'i18next';
-import { UIIcon, UiIcons } from '@sap-ux/ui-components';
+import { UIIcon, UiIcons, UICallout, UIIconButton, UITextInput } from '@sap-ux/ui-components';
 import { focusOnElement } from '../../utils';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { AppState } from '../../../../types';
+import { GuidedAnswerNode, GuidedAnswerTreeId } from '@sap/guided-answers-extension-types';
 
 /**
  *
@@ -67,6 +72,91 @@ export function RestartButton() {
                 {i18next.t('RESTART')}
             </span>
         </button>
+    );
+}
+
+/**
+ *
+ * @returns A button component for sharing a link.
+ */
+export function ShareButton() {
+    const id = 'callout-test-id';
+    const [isCalloutVisible, setCalloutVisible] = useState(false);
+    const [isCopiedVisible, setCopiedVisible] = useState(false);
+    const toggleCallout = (): void => {
+        setCopiedVisible(false);
+        setCalloutVisible(!isCalloutVisible);
+    };
+    const toggleCopied = (): void => {
+        setCopiedVisible(!isCopiedVisible);
+    };
+    const nodes = useSelector<AppState, GuidedAnswerNode[]>((state) => state.activeGuidedAnswerNode);
+    const treeId = useSelector<AppState, GuidedAnswerTreeId>((state) => state.activeGuidedAnswer!.TREE_ID);
+    const [link, setLink] = useState('');
+
+    useEffect(() => {
+        setLink(
+            `vscode://saposs.sap-guided-answers-extension#/tree/${treeId}/actions/${nodes
+                .map((n) => n.NODE_ID)
+                .join(':')}`
+        );
+    }, [nodes, treeId]);
+
+    return (
+        <div>
+            <UIIconButton
+                className="guided-answer__header__navButtons guided-answer__header__navButtons__content"
+                id={id}
+                iconProps={{ iconName: UiIcons.Link }}
+                checked={isCalloutVisible}
+                onClick={toggleCallout}></UIIconButton>
+            {isCalloutVisible && (
+                <UICallout
+                    target={`#${id}`}
+                    isBeakVisible={true}
+                    beakWidth={10}
+                    directionalHint={DirectionalHint.bottomCenter}
+                    onDismiss={() => toggleCallout()}
+                    calloutWidth={230}
+                    calloutMinWidth={230}>
+                    <div>
+                        <div style={{ margin: '10px 0 0 10px', fontSize: '13px', fontWeight: '700' }}>
+                            Share this answer
+                        </div>
+                        <div style={{ display: 'flex', margin: '10px' }}>
+                            {!isCopiedVisible && <UITextInput value={link} style={{ width: '184px' }} />}
+                            {isCopiedVisible && (
+                                <div
+                                    style={{
+                                        width: '186px',
+                                        border: '1px solid var(--vscode-terminal-ansiGreen)',
+                                        lineHeight: '22px',
+                                        paddingLeft: '6px',
+                                        fontSize: '13px',
+                                        display: 'inline-block'
+                                    }}>
+                                    <UIIcon
+                                        iconName={UiIcons.ConfirmationCheckSymbol}
+                                        style={{ marginRight: '5px', verticalAlign: 'middle' }}
+                                    />
+                                    Copied to clipboard
+                                </div>
+                            )}
+                            <CopyToClipboard text={link} onCopy={toggleCopied}>
+                                <UIIcon
+                                    className="copy-to-clipboard"
+                                    iconName={UiIcons.Copy}
+                                    style={{ padding: '5px 0 0 10px', cursor: 'pointer' }}
+                                />
+                            </CopyToClipboard>
+                        </div>
+                        <p style={{ padding: '0 10px 0 10px', fontSize: '11px' }}>
+                            Paste this code into search field on the home page to get back here.
+                        </p>
+                    </div>
+                </UICallout>
+            )}
+        </div>
     );
 }
 
