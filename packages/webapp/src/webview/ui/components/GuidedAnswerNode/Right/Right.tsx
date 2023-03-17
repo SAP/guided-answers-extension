d32a5bd980cdd38f105d6fabed1022fd01027421
Olivers Berzs <105420446+oliversberzs@users.noreply.github.com>
2023-03-17 11:33:07 +0200

    feat(310): change node enhancement design (#431)
    * feat(310): change node enhancement design
    
    * Create wise-donuts-smoke.md
    
    * feat(310): fix clickable area

--- packages/webapp/src/webview/ui/components/GuidedAnswerNode/Right/Right.tsx
+++ packages/webapp/src/webview/ui/components/GuidedAnswerNode/Right/Right.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from 'react';
 import { actions } from '../../../../state';
 import type { GuidedAnswerNode as GuidedAnswerNodeType } from '@sap/guided-answers-extension-types';
+import { UiIcons, UIIcon } from '@sap-ux/ui-components';
 import '../GuidedAnswerNode.scss';
 import { FocusZone, FocusZoneDirection } from '@fluentui/react-focus';
 
@@ -16,17 +17,24 @@ export function Right(props: { activeNode: GuidedAnswerNodeType }): ReactElement
                 <FocusZone direction={FocusZoneDirection.vertical} isCircularNavigation={true}>
                     {props.activeNode.COMMANDS
                         ? props.activeNode.COMMANDS.map((command) => (
-                              <div className="guided-answer__node__command" key={`command-${command.label}`}>
+                              <div
+                                  className="guided-answer__node__command"
+                                  key={`command-${command.label}`}
+                                  title={command.label}
+                                  onClick={() => {
+                                      actions.executeCommand(command);
+                                  }}>
                                   <div className="guided-answer__node__command__header">
+                                      <UIIcon
+                                          className="guided-answer__node__command__header__icon"
+                                          iconName={UiIcons.CreateMockData}
+                                      />
                                       <div className="guided-answer__node__command__header__label">{command.label}</div>
                                   </div>
 
                                   <button
                                       className="guided-answer__node__command__description"
-                                      id="guided-answer__node__command"
-                                      onClick={(): void => {
-                                          actions.executeCommand(command);
-                                      }}>
+                                      id="guided-answer__node__command">
                                       {command.description}
                                   </button>
                               </div>
