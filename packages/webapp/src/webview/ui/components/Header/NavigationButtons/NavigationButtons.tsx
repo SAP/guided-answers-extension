55c97c4a1a14a07a74e0e3b2f2a709236771b29c
Christos Koutsiaris <christos.koutsiaris@sap.com>
2023-05-22 11:39:36 +0100

    feat(ga): eslint fix

--- packages/webapp/src/webview/ui/components/Header/NavigationButtons/NavigationButtons.tsx
+++ packages/webapp/src/webview/ui/components/Header/NavigationButtons/NavigationButtons.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { DirectionalHint } from '@fluentui/react';
 import { actions } from '../../../../state';
