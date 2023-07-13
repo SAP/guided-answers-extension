a9282c2ada2a26015b1db5c5b0667e4ae79132cb
Olivers Berzs <olivers.berzs@sap.com>
2023-07-13 16:43:53 +0300

    feat(232): fix code smells

--- packages/webapp/src/webview/ui/components/GuidedAnswerNode/Middle/Middle.tsx
+++ packages/webapp/src/webview/ui/components/GuidedAnswerNode/Middle/Middle.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import type { ReactElement } from 'react';
 import { actions } from '../../../../state';
 import type { GuidedAnswerNode } from '@sap/guided-answers-extension-types';
@@ -11,7 +11,6 @@ import { FeedbackSection } from '../../FeedbackSection/FeedbackSection';
 import NotSolvedMessage from '../../FeedbackSection/NotSolvedMessage/NotSolvedMessage';
 import { FeedbackDialogBox } from '../../DialogBoxes/FeedbackDialogBox';
 import { FeedbackSentDialogBox } from '../../DialogBoxes/FeedbackSentDialogBox/FeedbackSentDialogBox';
-import { useEffect } from 'react';
 
 let firstTimeFocus = true;
 
@@ -30,11 +29,6 @@ export function Middle(props: {
     const appState = useSelector<AppState, AppState>((state) => state);
     firstTimeFocus = true;
 
-    if (!appState.activeGuidedAnswer) {
-        // No active tree, nothing we can do here
-        return <></>;
-    }
-
     useEffect(() => {
         if (appState.activeGuidedAnswer) {
             actions.updateLastVisitedGuide([
@@ -48,58 +42,63 @@ export function Middle(props: {
         }
     }, [props.activeNode.NODE_ID]);
 
-    return appState.guideFeedback === false ? (
-        <NotSolvedMessage />
-    ) : (
-        <div id="middle">
-            <div className="body_container">
-                <header>{props.activeNode.TITLE}</header>
-                <div id="hr"></div>
-                {props.enhancedBody ? (
-                    <FocusZone direction={FocusZoneDirection.vertical} isCircularNavigation={true}>
-                        {props.enhancedBody}
-                    </FocusZone>
-                ) : (
-                    <FocusZone direction={FocusZoneDirection.vertical} isCircularNavigation={true}>
-                        <div className="content" dangerouslySetInnerHTML={{ __html: props.activeNode.BODY }}></div>
-                    </FocusZone>
-                )}
-                <p className="guided-answer__node__question">{props.activeNode.QUESTION}</p>
-            </div>
-            <FocusZone
-                role="listbox"
-                onFocus={() => {
-                    if (firstTimeFocus) {
-                        focusOnElement('.guided-answer__node__edge:first-child');
-                        firstTimeFocus = false;
-                    }
-                }}
-                direction={FocusZoneDirection.bidirectional}
-                isCircularNavigation={true}>
-                <div className="guided-answer__node">
-                    {props.activeNode.EDGES.length > 0
-                        ? props.activeNode.EDGES.map((edge) => (
-                              <button
-                                  role="option"
-                                  key={`edge_button${edge.TARGET_NODE}`}
-                                  className="guided-answer__node__edge"
-                                  id="edge_button"
-                                  onClick={(): void => {
-                                      actions.selectNode(edge.TARGET_NODE);
-                                      focusOnElement('.home-icon');
-                                  }}>
-                                  {edge.LABEL}
-                              </button>
-                          ))
-                        : appState.activeGuidedAnswer && appState.activeGuidedAnswerNode && <FeedbackSection />}
+    if (!appState.activeGuidedAnswer) {
+        // No active tree, nothing we can do here
+        return <></>;
+    } else if (!appState.guideFeedback) {
+        return <NotSolvedMessage />;
+    } else {
+        return (
+            <div id="middle">
+                <div className="body_container">
+                    <header>{props.activeNode.TITLE}</header>
+                    <div id="hr"></div>
+                    {props.enhancedBody ? (
+                        <FocusZone direction={FocusZoneDirection.vertical} isCircularNavigation={true}>
+                            {props.enhancedBody}
+                        </FocusZone>
+                    ) : (
+                        <FocusZone direction={FocusZoneDirection.vertical} isCircularNavigation={true}>
+                            <div className="content" dangerouslySetInnerHTML={{ __html: props.activeNode.BODY }}></div>
+                        </FocusZone>
+                    )}
+                    <p className="guided-answer__node__question">{props.activeNode.QUESTION}</p>
                 </div>
-            </FocusZone>
-            {appState.activeGuidedAnswer && appState.activeGuidedAnswerNode && (
-                <>
-                    <FeedbackDialogBox />
-                    <FeedbackSentDialogBox />
-                </>
-            )}
-        </div>
-    );
+                <FocusZone
+                    role="listbox"
+                    onFocus={() => {
+                        if (firstTimeFocus) {
+                            focusOnElement('.guided-answer__node__edge:first-child');
+                            firstTimeFocus = false;
+                        }
+                    }}
+                    direction={FocusZoneDirection.bidirectional}
+                    isCircularNavigation={true}>
+                    <div className="guided-answer__node">
+                        {props.activeNode.EDGES.length > 0
+                            ? props.activeNode.EDGES.map((edge) => (
+                                  <button
+                                      role="option"
+                                      key={`edge_button${edge.TARGET_NODE}`}
+                                      className="guided-answer__node__edge"
+                                      id="edge_button"
+                                      onClick={(): void => {
+                                          actions.selectNode(edge.TARGET_NODE);
+                                          focusOnElement('.home-icon');
+                                      }}>
+                                      {edge.LABEL}
+                                  </button>
+                              ))
+                            : appState.activeGuidedAnswer && appState.activeGuidedAnswerNode && <FeedbackSection />}
+                    </div>
+                </FocusZone>
+                {appState.activeGuidedAnswer && appState.activeGuidedAnswerNode && (
+                    <>
+                        <FeedbackDialogBox />
+                        <FeedbackSentDialogBox />
+                    </>
+                )}
+            </div>
+        );
+    }
 }
