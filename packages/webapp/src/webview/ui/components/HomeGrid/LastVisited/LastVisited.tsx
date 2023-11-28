d1a1051bff05178aece59836d9c3be2d7564d7f9
Christos Koutsiaris <christos.koutsiaris@sap.com>
2023-11-28 09:58:25 +0000

    fix(ga): a11y issues

--- packages/webapp/src/webview/ui/components/HomeGrid/LastVisited/LastVisited.tsx
+++ packages/webapp/src/webview/ui/components/HomeGrid/LastVisited/LastVisited.tsx
@@ -6,7 +6,7 @@ import { actions } from '../../../../state';
 import { FocusZone, FocusZoneDirection } from '@fluentui/react-focus';
 import { UIIcon, UiIcons } from '@sap-ux/ui-components';
 import { TreeItemBottomSection } from '../../TreeItemBottomSection';
-import type { LastVisitedGuide } from '@sap/guided-answers-extension-types';
+import type { GuidedAnswerNode, LastVisitedGuide } from '@sap/guided-answers-extension-types';
 import './LastVisited.scss';
 
 /**
@@ -16,6 +16,19 @@ import './LastVisited.scss';
  */
 export function LastVisited(): ReactElement {
     const lastVisitedGuides = useSelector<AppState, LastVisitedGuide[]>((state) => state.lastVisitedGuides);
+
+    /**
+     * Handles the event for the last visited link in a guided answer interface.
+     *
+     * @param {LastVisitedGuide} guide - An object representing the last visited guide,
+     * which includes the guided answer tree, the path of nodes visited, and the creation timestamp.
+     */
+    function lastVisitedLinkHandler(guide: LastVisitedGuide) {
+        actions.setActiveTree(guide.tree);
+        guide.nodePath.forEach((node: GuidedAnswerNode) => actions.updateActiveNode(node));
+        document.body.focus();
+    }
+
     return (
         <div>
             <h3 className="guided-answer__home-grid__section__title">
@@ -34,9 +47,12 @@ export function LastVisited(): ReactElement {
                                 <a
                                     id="last-visited-link"
                                     onClick={(): void => {
-                                        actions.setActiveTree(guide.tree);
-                                        guide.nodePath.forEach((node: any) => actions.updateActiveNode(node));
-                                        document.body.focus();
+                                        lastVisitedLinkHandler(guide);
+                                    }}
+                                    onKeyDown={(event) => {
+                                        if (event.key === 'Enter') {
+                                            lastVisitedLinkHandler(guide);
+                                        }
                                     }}>
                                     {guideTitle}
                                 </a>
